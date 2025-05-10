8b61613397516174a4477b8169a7a5c252049c27
akotetsu <yuanliqu@gmail.com>
2025-05-10 13:45:43 +0900

    Initial Commit

---
+++ API1/API1App.swift
@@ -0,0 +1,17 @@
+//
+//  API1App.swift
+//  API1
+//
+//  Created by 原里駆 on 2025/05/10.
+//
+
+import SwiftUI
+
+@main
+struct API1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API1
+//
+//  Created by 原里駆 on 2025/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
